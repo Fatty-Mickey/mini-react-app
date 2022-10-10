2b94a6a7e46857aa036b6675e43dd929a20862a2
Maksym Bilyk <109813957+Fatty-Mickey@users.noreply.github.com>
2022-10-10 19:38:45 +0300

    Add count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import CountFunctionComponent from './components/CountFunctionComponent';
+import CountClassComponent from './components/CountClassComponent';
 import Component3 from './components/Component3';
 import Component4 from './components/Component4';
 
 function App() {
   return (
     <>
+      <CountFunctionComponent />
+      <hr />
+      <CountClassComponent />
+      <hr />
       <Component3 />
       <hr />
       <Component4 />
--- src/components/Component3.js
+++ src/components/Component3.js
@@ -1,16 +1,11 @@
 import React, { useState } from 'react'
 
 export default function Component3() {
-    const [count, setCount] = useState(0)
     const [comments, setComment] = useState([])
 
     let myRef = React.createRef()
 
-    let handler = () => {
-        let currentCount = count
-        currentCount++
-        setCount(currentCount)
-    }
+
     let addComment = () => {
         let currentComment = myRef.current.value
         let updateComments = [...comments, currentComment]
@@ -20,10 +15,6 @@ export default function Component3() {
     return (
         <div>
             <h1>State</h1>
-            <div>
-                <button onClick={handler}>Count +</button>
-            </div>
-            <div>{count}</div>
             <div><textarea ref={myRef}></textarea></div>
             <div>
                 <button onClick={addComment}>Add comment</button>
